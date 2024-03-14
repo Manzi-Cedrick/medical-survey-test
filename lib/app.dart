b818904709a95866119e8fcedabf29bab2275a60
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-14 02:20:01 +0200

    [core]: bloc transition from GetX

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,14 @@
-import 'package:app_test/features/authentication/screens/create-account/create_account_screen.dart';
+import 'package:app_test/features/authentication/screens/auth/create-account/create_account_screen.dart';
 import 'package:app_test/utils/constants/text_strings.dart';
 import 'package:app_test/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: TTexts.appName,
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
