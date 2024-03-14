8370127e8e606cc8cc73a54587f1878575ec147d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-14 15:08:19 +0200

    [core]: date functioning

--- lib/features/authentication/screens/survey-format/occupation/starter_screen_dob.dart
+++ lib/features/authentication/screens/survey-format/occupation/starter_screen_dob.dart
@@ -3,9 +3,7 @@ import 'package:app_test/common/section/previous_continue.dart';
 import 'package:app_test/common/section/primary_section.dart';
 import 'package:app_test/features/authentication/screens/survey-format/occupation/01_first_screen_occupation.dart';
 import 'package:app_test/features/authentication/screens/survey-format/occupation/bloc/survey_form_bloc.dart';
-import 'package:app_test/utils/constants/colors.dart';
 import 'package:app_test/utils/constants/sizes.dart';
-import 'package:date_picker_plus/date_picker_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,6 +15,7 @@ class DobScreen extends StatefulWidget {
 }
 
 class _DobScreenState extends State<DobScreen> {
+  String? _selectedDate;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,36 +37,41 @@ class _DobScreenState extends State<DobScreen> {
                 ),
                 const SizedBox(height: TSizes.spaceBtwSections),
                 BlocConsumer<SurveyFormBloc, SurveyFormState>(
-                  listener: (context, state) {
-                    
-                  },
+                  listener: (context, state) {},
                   builder: (context, state) {
-                    return DatePicker(
-                        minDate: DateTime(1900, 1, 1),
-                        maxDate: DateTime.now(),
-                        initialDate: DateTime.now(),
-                        onDateSelected: (value) {
-                          context.read<SurveyFormBloc>().add(
-                              SurveyFormDateTimeEvent(value));
-                        },
-                        highlightColor: TColors.primary,
-                        splashColor: TColors.primary,
+                    return TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          DateTime? parsedDate = DateTime.tryParse(value);
+                          if (parsedDate != null) {
+                            print(parsedDate);
+                            context.read<SurveyFormBloc>().add(
+                                  SurveyFormDateTimeEvent(parsedDate),
+                                );
+                          }
+                        });
+                      },
+                      keyboardType: TextInputType.datetime,
+                      decoration: InputDecoration(
+                        labelText: 'Date',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.calendar_today),
+                          onPressed: () {
+                            showDatePicker(context: context,initialDate: DateTime.now(), firstDate: DateTime(2017,9,1), lastDate: DateTime.now());
+                          },
+                        ),
+                      ),
                     );
                   },
                 ),
                 const SizedBox(height: TSizes.spaceBtwSections),
                 TSectionFooterButtons(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-
-                        MaterialPageRoute(
-                            builder: (_) {
-                              return BlocProvider(create: (context) => SurveyFormBloc(),
-                              child: const OccupationDetailScreen());
-                            }
-                        )
-                    );
+                    Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return BlocProvider(
+                          create: (context) => SurveyFormBloc(),
+                          child: const OccupationDetailScreen());
+                    }));
                   },
                 )
               ],
