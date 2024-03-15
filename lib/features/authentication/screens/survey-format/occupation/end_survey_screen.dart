7aec9c2c74f66ae33f04589a825bcf90f97b854b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-15 12:01:01 +0200

    [core]: back navigation disabling --addition

--- lib/features/authentication/screens/survey-format/occupation/end_survey_screen.dart
+++ lib/features/authentication/screens/survey-format/occupation/end_survey_screen.dart
@@ -3,32 +3,40 @@ import 'package:app_test/utils/constants/colors.dart';
 import 'package:app_test/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
-class EndSurveyScreen extends StatelessWidget {
+class EndSurveyScreen extends StatefulWidget {
   const EndSurveyScreen({super.key});
+
+  @override
+  _EndSurveyScreen createState() => _EndSurveyScreen();
+}
+class _EndSurveyScreen extends State<EndSurveyScreen> {
   @override
   Widget build(BuildContext context) {
-    return TPrimarySectionLayout(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(TSizes.defaultSpace),
-            decoration: BoxDecoration(
-                border: Border.all(color: TColors.primary),
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.transparent),
-            child: Center(
-              child: Text(
-                'Thank you for completing the survey! The answers you provided will develop into data! This data can help administer a better, more personal experience with your AI',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium!
-                    .apply(color: TColors.primary),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: TPrimarySectionLayout(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              decoration: BoxDecoration(
+                  border: Border.all(color: TColors.primary),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.transparent),
+              child: Center(
+                child: Text(
+                  'Thank you for completing the survey! The answers you provided will develop into data! This data can help administer a better, more personal experience with your AI',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .apply(color: TColors.primary),
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
