ed1bdd346b6542f38e5637b4941ab970e2cc91f6
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-15 10:41:51 +0200

    [core]: sizes changes --addition

--- lib/common/style/spacing_style.dart
+++ lib/common/style/spacing_style.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class TSpacingStyle {
   static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
-      top: TSizes.appBarHeight,
+      top: TSizes.sm,
       left: TSizes.defaultSpace,
       bottom: TSizes.defaultSpace,
       right: TSizes.defaultSpace);
